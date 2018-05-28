3b22ed4f34e19e7126dbab765f29fbba78c72bd6
codepgq <devpgq@126.com>
2018-05-28 17:37:24 +0800

    init

---
+++ PQTransitionDemo/Transition/Transition.h
@@ -0,0 +1,21 @@
+//
+//  Transition.h
+//  Transition
+//
+//  Created by 盘国权 on 2018/5/28.
+//  Copyright © 2018年 pgq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transition.
+FOUNDATION_EXPORT double TransitionVersionNumber;
+
+//! Project version string for Transition.
+FOUNDATION_EXPORT const unsigned char TransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transition/PublicHeader.h>
+
+#import "PQTransition.h"
+#import "PQPresentationController.h"
+
